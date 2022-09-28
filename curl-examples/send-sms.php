9d04199165cb28a14e78acace0606ec9a02e9ea2
Ardary <112001960+ArdarySolo@users.noreply.github.com>
2022-09-28 15:46:57 +0100

    Create send-sms.php

---
+++ curl-examples/send-sms.php
@@ -0,0 +1,24 @@
+<?php
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://www.ardary-sms.com/api/addcontactSolo',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{"header":{"login":"user@gmail.com","password":"test","api":true},"contacts":[{"first_name":"Joe","last_name":"","phone_number":"668xxxxxx","country_code":"FR","email":""}]}',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+?>
