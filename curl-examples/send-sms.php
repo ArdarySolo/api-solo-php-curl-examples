84775363ad1b28ed4f7f4462742b4c0a4c5ab7c7
Ardary-inSIM <112001960+ArdaryinSIM@users.noreply.github.com>
2023-12-19 16:47:31 +0100

    Update send-sms.php

--- curl-examples/send-sms.php
+++ curl-examples/send-sms.php
@@ -2,7 +2,7 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://www.ardary-sms.com/api/addcontactSolo',
+  CURLOPT_URL => 'https://www.ardary-sms.com/api/sendSmsSolo',
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
